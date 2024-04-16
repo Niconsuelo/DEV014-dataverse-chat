52c93f1722bdbeec15e4236f3673e45d1fdffad4
Milen <millalen.pg@gmail.com>
2024-04-16 14:27:51 -0400

    Implementación estructura filterBar, refactorizado en un componente

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,6 +2,7 @@
 Esta función debe devolver un elemento DOM para que pueda ser utilizado por el enrutador.*/
 
 import { cards } from "../components/Cards.js";
+import { filterBar } from "../components/filterBar.js";
 import { stats } from "../components/stats.js";
 import dataset from "../data/dataset.js";
 
@@ -9,17 +10,22 @@ const Home = (props) => {
   console.log("AQUI ES HOME ", props);
   // HOME ES LA VISTA INICIAL-DEBE CONTENER TODO LO QUE SE VA A RENDERIZAR EN EL INICIO HTML, RENDERCARD DEBEN IR AQUI
   const viewEl = document.createElement("div");
-  const renderCards = cards(dataset);
 
+  const controlBar = document.createElement("div");
+  controlBar.innerHTML = filterBar();
+  viewEl.appendChild(controlBar);
+
+  const renderCards = cards(dataset);
   viewEl.appendChild(renderCards);
   //viewEl.textContent = `Welcome to the home page! ${props.bella}`;
 
   const newSection = document.createElement("section");
   const renderStats = stats(dataset);
   newSection.innerHTML = renderStats;
-
   viewEl.appendChild(newSection);
 
+ 
+  
   return viewEl;
 };
 
