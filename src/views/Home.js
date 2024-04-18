d55fad7ae61486c556e6c0462ece7d66a9875cc1
Milen <millalen.pg@gmail.com>
2024-04-17 22:26:09 -0400

    Event Listener de filtro ocupación check

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,6 +5,7 @@ import { cards } from "../components/Cards.js";
 import { filterBar } from "../components/filterBar.js";
 import { stats } from "../components/stats.js";
 import dataset from "../data/dataset.js";
+import { filterData, sortData } from "../lib/dataFunctions.js";
 
 const Home = (props) => {
   console.log("AQUI ES HOME ", props);
@@ -15,16 +16,39 @@ const Home = (props) => {
   controlBar.innerHTML = filterBar();
   viewEl.appendChild(controlBar);
 
+  const cardsContainer = document.createElement("div"); // *CREAR UN NUEVO CONTENEDOR EXCLUSIVO PARA EL UL
   const renderCards = cards(dataset);
-  viewEl.appendChild(renderCards);
+  cardsContainer.appendChild(renderCards); // *AL CONTENEDOR CREADO, INSERTAMOS EL UL 
+  viewEl.appendChild(cardsContainer); // *AL CONTENEDOR PADRE DE NUESTRA VISTA HOME.JS (SUCESOR DE ROOT) LE INSERTAMOS EL CONTENEDOR CON EL UL
   //viewEl.textContent = `Welcome to the home page! ${props.bella}`;
 
+  const selectfilterOcupation = viewEl.querySelector("#filter-ocupation"); // No sirve la referencia a document, hay que cambiarla a viewEl que es contenedor padre
+  const nenSelect = viewEl.querySelector("#select-filter");
+  const sortSelect = viewEl.querySelector("#sort-select");
+
+  selectfilterOcupation.addEventListener("change", function (event) {
+    let result = [];
+    const optionFilter = event.target.value;
+    result = filterData(dataset, "ocupation", optionFilter);
+    const nenTypeSelect = nenSelect.value;
+    if (nenTypeSelect !== "") {
+      result = filterData(result, "nenType", nenTypeSelect);
+    }
+    const sortSelection = sortSelect.value;
+    if (sortSelection !== "") {
+      result = sortData(result, "name", sortSelection);
+    }
+    // const root = document.querySelector("#root");  *NO LA NECESITAMOS PORQ YA TENEMOS LA REFERENCIA AL CONTENEDOR PADRE DE HOME
+    cardsContainer.innerHTML = "";
+    cardsContainer.appendChild(cards(result));
+  });
+  
   const newSection = document.createElement("section");
   const renderStats = stats(dataset);
   newSection.innerHTML = renderStats;
   viewEl.appendChild(newSection);
 
- 
+
   
   return viewEl;
 };
