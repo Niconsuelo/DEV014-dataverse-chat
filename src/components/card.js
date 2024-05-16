5b6ef835d20491b89dab1c1f3c93f7118be2f05f
Milen <millalen.pg@gmail.com>
2024-05-16 14:28:37 -0400

    Readme. Últimas features en vista Home: hover para click-img

--- src/components/card.js
+++ src/components/card.js
@@ -10,7 +10,6 @@ export const card = (character) => {
           <span>Chatea conmigo...</span>
           <a id="click-img">
             <img src=${character.imageUrl} alt=${character.name} class="card-img">
-            
           </a>
           <img src="https://d100mj7v0l85u5.cloudfront.net/s3fs-public/2022-10/futuro-codigo-de-barras.png" alt="barcode" class="barcode-img">
         </div> 
